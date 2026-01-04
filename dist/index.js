dc1400b6a97708383f7fece71b3cbfdaa1e5700d
Quentin90000 <156473079+Quentin90000@users.noreply.github.com>
2026-01-04 16:48:58 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -180,8 +180,8 @@ async function request(path, options = {}) {
                     'Content-Type': 'application/x-www-form-urlencoded'
                 },
                 body: new URLSearchParams({
-                    'username': 'smeagolworms4@gmail.com',
-                    'password': 'O2range!',
+                    'username': `${aldesLogin}`,
+                    'password': `${aldesPassword}`,
                     'grant_type': 'password'
                 })
             })
@@ -242,14 +242,14 @@ function sendCommand(id, method, value) {
 }
 const devices = {};
 const modeMapping = {
-    A: 'auto',
+    X: 'guest',
     Z: 'programming',
     V: 'daily',
     W: 'vacation',
     Y: 'boost',
 };
 const modeRMapping = {
-    auto: 'A',
+    guest: 'X',
     programming: 'Z',
     daily: 'V',
     vacation: 'W',
@@ -537,7 +537,7 @@ async function analyseProduct(product) {
                     state_topic: `${mqttPrefix}/${details.serial_number}/week_planning`,
                     value_template: `{{ value_json.${d}.h${h} }}`,
                     command_topic: `${mqttPrefix}/${details.serial_number}/set_week_planning_${d}_${h}`,
-                    options: ['daily', 'vacation', 'boost'],
+                    options: ['daily', 'vacation', 'boost', 'guest'],
                     device: device.mqqtDevice,
                 }));
             }
